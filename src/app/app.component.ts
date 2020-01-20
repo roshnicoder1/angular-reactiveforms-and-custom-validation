1de599c9f6331fa486b7c0c5fe24e85b2948230b
deepan.ramalingam <deepan.ramalingam@stackroute.in>
2020-01-20 13:42:22 +0530

    upto reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-demo';
+  username: FormControl = new FormControl('',[this.ageRangeValidator]);
+  password: FormControl = new FormControl('');
+
+  loginForm: FormGroup = new FormGroup({
+  username:this.username,
+  password:this.password
+});
+
+
+  constructor(){
+    this.loginForm.controls.username.valueChanges.subscribe(()=>{
+      console.log(this.loginForm.controls.username.status);
+      console.log(this.loginForm.controls.username);
+    })
+  }
+  login(){
+    // event.preventDefault();
+    console.log(this.loginForm.value.username);
+    // console.log(this.username);
+    // console.log(this.password);
+  }
+
+  ageRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    if (control.value !== undefined && (isNaN(control.value) || control.value < 18 || control.value > 45)) {
+        return { 'ageFilter': true };
+    }
+    return null;
+}
 }
